84f94e76f9ae5a2660897435f7e2f699d11341d0
Arjun-Arora <aarora52@stanford.edu>
2018-11-17 15:01:42 -0800

    changed getitem to only take in index
    stilil doesnt work but commented out code contains my idea

--- src/util.py
+++ src/util.py
@@ -16,54 +16,48 @@ class VideoInterpTripletsDataset(torch.utils.data.Dataset):
         """
         self.videoFilenames = [filename for filename in glob.glob(os.path.join(videoDirectory,'*.mp4'))]
         self.tripletSize = tripletSize
-        self.asepectRatio = aspectRatio
+        self.aspectRatio = aspectRatio
         self.transform = transform
     def __len__(self):
         len = 0
         for filename in self.videoFilenames:
             len += int(skvideo.io.ffprobe(filename)['video']['@nb_frames'])
         return len
-    def __getitem__(self,videoFilename,idx):
+    def __getitem__(self,index):
         '''
-        :param videoFilename: filename of the video you want
-        :param idx: idx of the first frame in the video you want
+        :param index: idx of the first frame in the video you want
         :return:
         '''
-        if videoFilename in self.videoFilenames:
-            reader = skvideo.io.vreader(videoFilename,inputdict={'--start_number':str(idx),'-vframes':str(3)})
-            H = self.tripletSize * self.aspectRatio
-            W = self.tripletSize
-            videoTriplet = []
-            #each frame should be (H,W,C)
-            for frame in reader:
-                videoTriplet.append(frame)
-            #stackedInterpFrames (H,W,C,2)
-            stackedInterpFrames = np.stack([np.expand_dims(videoTriplet[0],axis=-1),
-                                            np.expand_dims(videoTriplet[2],axis=-1)],axis=-1)
-            #stackedInterpFrames (1,C,H,W,2)
-            stackedInterpFrames = np.transpose(np.expand_dims(stackedInterpFrames,axis=0),(0,3,1,2,4))
-            #missingFrame (1,C,H,W)
-            missingFrame = np.transpose(np.expand_dims(videoTriplet[1],axis = 0),(0,3,1,2))
-            # return a tuple of (stackedInterpFrames,missingFrame) #((1,C,H,W,2),(1,C,H,W))
-            return (stackedInterpFrames,missingFrame)
-        else:
-            return AssertionError("videoFile: {} does not exist in the dataset".format(videoFilename))
-        # tripletFilenames = [filename for filename in glob.glob(os.path.join(tripletDirectory,"*_triplets.mp4"))]
-        # for filename in videoFilenames:
-        #     if (filename[:-4] + '_triplets.mp4') in tripletFilenames:
-        #         continue
-        #     else:
-        #         #this will be a (3,N,C,H,W) Tensor
-        #         # N = # of frames / 3
-        #         # (0,N,C,H,W) = frame 1's of triplet
-        #         # (1,N,C,H,W) = frame 3's of triplet
-        #         # (2,N,C,H,W) = frame 2's of triplet
-        #         H = tripletSize * aspectRatio
-        #         W = tripletSize
-        #         videoTriplet = numpy.zeros_like([3,1,3,H,W])
-        #         videoData = skvideo.io.vreader(filename)
-        #         idx = 1
-        #         #count which frame this is
-        #         for frame in videoData:
-        #             if idx % 2 == 0:
-        #             else:
+        # totalLen = 0
+        # correctFilename = None
+        # for filename in self.videoFilenames:
+        #    lengthOfVideo = int(skvideo.io.ffprobe(filename)['video']['@nb_frames'])
+        #    if totalLen+lengthOfVideo-3 >= index:
+        #        correctFilename = filename
+        #        print(filename)
+        #        break
+        #    elif totalLen+lengthOfVideo-3 < index and totalLen + lengthOfVideo > index: #weird case
+        #        correctFilename = filename
+        #        # index - the amount needed to make sure you have at least 3 frames in triplet
+        #        index = index - (totalen+lengthOfVideo-index)
+        #        break
+        #    elif totalLen+lengthOfVideo-3 < index:
+        #        lenVideo += lengthOfVideo
+        # print(correctFilename)
+
+        reader = skvideo.io.vreader(correctFilename,inputdict={'--start_number':str(index),'-vframes':str(3)})
+        H = self.tripletSize * self.aspectRatio
+        W = self.tripletSize
+        videoTriplet = []
+        #each frame should be (H,W,C)
+        for frame in reader:
+            videoTriplet.append(frame)
+        #stackedInterpFrames (H,W,C,2)
+        stackedInterpFrames = np.stack([np.expand_dims(videoTriplet[0],axis=-1),
+                                        np.expand_dims(videoTriplet[2],axis=-1)],axis=-1)
+        #stackedInterpFrames (1,C,H,W,2)
+        stackedInterpFrames = np.transpose(np.expand_dims(stackedInterpFrames,axis=0),(0,3,1,2,4))
+        #missingFrame (1,C,H,W)
+        missingFrame = np.transpose(np.expand_dims(videoTriplet[1],axis = 0),(0,3,1,2))
+        # return a tuple of (stackedInterpFrames,missingFrame) #((1,C,H,W,2),(1,C,H,W))
+        return stackedInterpFrames,missingFrame
