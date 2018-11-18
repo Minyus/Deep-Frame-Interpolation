830951aec51cdde39108554effef2bbe8e417d12
Arjun-Arora <aarora52@stanford.edu>
2018-11-18 02:13:23 -0800

    made python notebook for runGAN
    made runGAN.ipynb so we can see plot outputs during train

--- src/train.py
+++ src/train.py
@@ -4,14 +4,25 @@ from torchvision import datasets
 import torchvision
 import matplotlib.pyplot as plt
 import sys
+import numpy as np
 sys.path.append("./src")
 sys.path.append("./models")
 from models.GAN_model import GANGenerator, GANDiscriminator
 
 def imshow(img):
-    npimg = img.numpy().transpose((1,2,0))
+    npimg = img.numpy().astype(np.uint8).transpose((1,2,0))
     #format H,W,C
     plt.imshow(npimg)
+    plt.show()
+
+def init_weights(m):
+    for param in m.parameters():
+        # print(param.shape)
+        if len(param.shape)>=2:
+            # print("before: {}".format(param.data[0]))
+            torch.nn.init.xavier_uniform(param)
+            # print("after: {}".format(param.data[0]))
+
 
 def trainGAN(epochs,dataloader):
     """
@@ -19,8 +30,22 @@ def trainGAN(epochs,dataloader):
     :param datasetloader: dataloader of dataset we want to train on
     :return: saved models
     """
+
     discriminator = GANDiscriminator()
     generator = GANGenerator()
+    dtype = torch.FloatTensor
+
+
+
+    if torch.cuda.is_available():
+        discriminator = discriminator.cuda()
+        generator = generator.cuda()
+        dtype = torch.cuda.FloatTensor
+
+    discriminator.apply(init_weights)
+    generator.apply(init_weights)
+
+
     D_optimizer = optim.Adam(discriminator.parameters(),lr=0.0002)
     G_optimizer = optim.Adam(generator.parameters(), lr=0.0002)
 
@@ -28,10 +53,9 @@ def trainGAN(epochs,dataloader):
 
     for epoch in range(epochs):
         for index, sample in enumerate(dataloader):
-            print(index)
-            #TODO(wizeng): add device = cpu or cuda code
+            # print(index)
             #inframes  (N,C,H,W,2), outframes (N,C,H,W)
-            left, right, outframes = sample['left'], sample['right'], sample['out']
+            left, right, outframes = sample['left'].type(dtype),sample['right'].type(dtype),sample['out'].type(dtype)
             inframes = (left, right)
 
             #train discriminator
@@ -40,22 +64,23 @@ def trainGAN(epochs,dataloader):
                                                           D_optimizer,
                                                           outframes,
                                                           generated_data,
-                                                          criterion)
+                                                          criterion,dtype)
 
             #train generator
             generated_data = generator(inframes)
-            G_loss = train_G(discriminator, G_optimizer, generated_data, criterion)
+            G_loss = train_G(discriminator, G_optimizer, generated_data, criterion,dtype)
 
             if index % 100 == 0:
-                # N = generated_data.shape[0]
-                # n_imgs = generated_data.data
-                # imshow(torchvision.utils.make_grid(n_imgs))
+                N = generated_data.shape[0]
+                n_imgs = generated_data.data.cpu()
+                imshow(torchvision.utils.make_grid(n_imgs))
+                imshow(torchvision.utils.make_grid(outframes.data.cpu()))
                 print("epoch {} out of {}".format(epoch,epochs))
                 print("D_loss:{}, G_loss:{}\n".format(D_loss,G_loss))
     return generator, discriminator
 
 
-def train_D(discriminator,optimizer,real_data,generated_data,criterion):
+def train_D(discriminator,optimizer,real_data,generated_data,criterion,dtype):
     """
     :param discriminator: discriminator model
     :param optimizer: optimizer object
@@ -68,18 +93,18 @@ def train_D(discriminator,optimizer,real_data,generated_data,criterion):
     N = real_data.shape[0]
 
     real_output = discriminator(real_data)
-    real_loss = criterion(real_output,torch.ones(N,1))
+    real_loss = criterion(real_output,torch.ones(N,1).type(dtype))
     real_loss.backward()
 
     generated_output = discriminator(generated_data)
-    gen_loss = criterion(generated_output,torch.zeros(N,1))
+    gen_loss = criterion(generated_output,torch.zeros(N,1).type(dtype))
     gen_loss.backward()
 
     optimizer.step()
 
     return real_loss+gen_loss,real_output,generated_output
 
-def train_G(discriminator,optimizer,generated_data,criterion):
+def train_G(discriminator,optimizer,generated_data,criterion,dtype):
     """
     :param discriminator: generator model
     :param optimizer: optimizer object
@@ -92,7 +117,7 @@ def train_G(discriminator,optimizer,generated_data,criterion):
     optimizer.zero_grad()
 
     output = discriminator(generated_data)
-    generated_loss = criterion(output,torch.ones(N,1))
+    generated_loss = criterion(output,torch.ones(N,1).type(dtype))
     generated_loss.backward()
 
     optimizer.step()
