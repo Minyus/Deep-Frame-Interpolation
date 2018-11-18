4a593e9ce333129b1a1ca20ccde648f93803b55c
William Zeng <wizeng@stanford.edu>
2018-11-17 21:48:00 -0600

    Finished utils.py and added video2frames.py

--- src/util.py
+++ src/util.py
@@ -1,33 +1,49 @@
+import glob
+import numpy as np
+import os
+from skimage.transform import resize
+import skvideo.io
 import torch
 from torch.utils.data import Dataset, DataLoader
-import os
-import glob
-import skvideo.io
-# import cv2
-import numpy as np
 
 class VideoInterpTripletsDataset(torch.utils.data.Dataset):
-    def __init__(self,videoDirectory,tripletSize=256,aspectRatio=16.0/9.0,transform=None):
+    def __init__(self, dir, height=144, width=256, transform=None):
         """
-        :param videoDirectory: Directory of videos
-        :param tripletSize = pixel width of tripletDataset
-        :param aspectRatio = aspect ratio of tripletDataset
+        :param dir: directory of videos
+        :param height = pixel height of frame
+        :param width = pixel width of frame
         :param transform:
         """
-        self.videoFilenames = [filename for filename in glob.glob(os.path.join(videoDirectory,'*.mp4'))]
-        self.tripletSize = tripletSize
-        self.aspectRatio = aspectRatio
+        self.filenames = [filename for filename in glob.glob(os.path.join(videoDirectory,'*.mp4'))]
+        self.height = height
+        self.width = width
         self.transform = transform
+        self.frames = [int(skvideo.io.ffprobe(f)['video']['@nb_frames']) for f in self.filenames]
+        self.total = sum(self.frames)
+
     def __len__(self):
-        len = 0
-        for filename in self.videoFilenames:
-            len += int(skvideo.io.ffprobe(filename)['video']['@nb_frames'])
-        return len
-    def __getitem__(self,index):
+        return self.total
+
+    def __getitem__(self, index):
         '''
         :param index: idx of the first frame in the video you want
-        :return:
+        :return: (inframes, outframes)((1,C,H,W,2),(1,C,H,W))
         '''
+
+        # iterate through video's frame lengths to the correct video
+        file = 0
+        while self.frames[file] <= index:
+            index -= self.frames[file]
+            file += 1
+        # if index near end of file, make sure it has enough frames to read
+        index = min(index, self.frames[file] - 3)
+
+        reader = skvideo.io.vreader(self.filenames[file], inputdict={'--start_number':str(index),'-vframes':str(3)})
+        triplet = [resize(frame, (self.height, self.width)) for frame in reader]
+        triplet = [np.transpose(frame, (2, 1, 0))[None] for frame in triplet] # (1, C, H, W)
+        inframes = np.stack([triplet[0], triplet[2]], axis=-1) # (1, C, H, W, 2)
+        return inframes, triplet[1]
+
         # totalLen = 0
         # correctFilename = None
         # for filename in self.videoFilenames:
@@ -44,20 +60,3 @@ class VideoInterpTripletsDataset(torch.utils.data.Dataset):
         #    elif totalLen+lengthOfVideo-3 < index:
         #        lenVideo += lengthOfVideo
         # print(correctFilename)
-
-        reader = skvideo.io.vreader(correctFilename,inputdict={'--start_number':str(index),'-vframes':str(3)})
-        H = self.tripletSize * self.aspectRatio
-        W = self.tripletSize
-        videoTriplet = []
-        #each frame should be (H,W,C)
-        for frame in reader:
-            videoTriplet.append(frame)
-        #stackedInterpFrames (H,W,C,2)
-        stackedInterpFrames = np.stack([np.expand_dims(videoTriplet[0],axis=-1),
-                                        np.expand_dims(videoTriplet[2],axis=-1)],axis=-1)
-        #stackedInterpFrames (1,C,H,W,2)
-        stackedInterpFrames = np.transpose(np.expand_dims(stackedInterpFrames,axis=0),(0,3,1,2,4))
-        #missingFrame (1,C,H,W)
-        missingFrame = np.transpose(np.expand_dims(videoTriplet[1],axis = 0),(0,3,1,2))
-        # return a tuple of (stackedInterpFrames,missingFrame) #((1,C,H,W,2),(1,C,H,W))
-        return stackedInterpFrames,missingFrame
