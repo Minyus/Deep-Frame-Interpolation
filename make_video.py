5bd7feb39bc34c92cb5c09e5627862a0a12bf1d0
William Zeng <wizeng@stanford.edu>
2018-12-10 21:25:58 -0800

    Mixes frames

--- make_video.py
+++ make_video.py
@@ -1,19 +1,30 @@
 import argparse
 import cv2
+from models.UNet_model import UNetGenerator
+import numpy as np
 from src.utils import VideoInterpTripletsDataset
 import sys
 import torch
 sys.path.append("./models")
 
-def tensor2numpy(tensor):
-    t = tensor.numpy().transpose((1,2,0))
-    return np.interp(output_image,(-1.0,1.0),(0,255.0)).astype(np.uint8)
+def write2video(tensor, vid):
+    t = tensor.squeeze().numpy().transpose((1,2,0))
+    npt = np.interp(t, (-1.0,1.0), (0,255.0)).astype(np.uint8)
+    cvt = cv2.cvtColor(npt, cv2.COLOR_RGB2BGR)
+    vid.write(cvt)
+
+def mix_frames(real, gen):
+    width = real.shape[-1]
+    gen[:, :, :, width / 2] = real[:, :, :, width / 2]
+    gen[:, :, :, width / 2 : width / 2 + 2] = -1
+    return gen
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('frames', help='path to frames')
     parser.add_argument('video', help='path to video')
     parser.add_argument('generator', help='path to generator')
+    # parser.add_argument('mix', help='mix output between two frames', default=True)
     args = parser.parse_args()
 
     cam = cv2.VideoCapture(args.video)
@@ -22,10 +33,9 @@ def main():
     dataset = VideoInterpTripletsDataset(args.frames, read_frames=True)
     height, width = dataset.getsize()
     gen = torch.load(args.generator, map_location='cpu')
-    print('Model Architecture Generator: ')
-    for name, param in gen.named_parameters():
-        if param.requires_grad:
-            print(name)
+    torch.save(gen.module.state_dict(), 'model')
+    gen = UNetGenerator()
+    gen.load_state_dict(torch.load('model'))
     dtype = torch.FloatTensor
     if torch.cuda.is_available():
         gen = gen.cuda()
@@ -35,14 +45,15 @@ def main():
     out = cv2.VideoWriter('out.mp4', fourcc, fps, (width, height))
     with torch.no_grad():
         for i in range(0, len(dataset) - 2, 2):
+            if i % 100 == 0:
+                print(i)
             data = {f:dataset[i][f][None, :] for f in dataset[i]}
             inframes = (data['left'], data['right'])
-            g = gen(inframes)
-            outframe = tensor2numpy(g)
             if i == 0:
-                out.write(tensor2numpy(inframes[0]))
-            out.write(outframe)
-            out.write(tensor2numpy(inframes[1]))
+                tensor2cv(inframes[0], out)
+            g = gen(inframes)
+            tensor2cv(mix_frames(data['out'], g), out)
+            tensor2cv(inframes[1], out)
     out.release()
     cv2.destroyAllWindows()
 
