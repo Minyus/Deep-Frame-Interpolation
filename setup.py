69feadfc91c5bd28bc03214e3d5fffee151a739e
Minyus <yusuke.minami86@gmail.com>
2019-04-20 19:47:59 +0800

    no message

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='deepframeinterpolation',
-      version='0.0.1',
-      description='Deep Frame Interpolation',
-      author='Arjun-Arora',
-      author_email='me@GitHub.com',
-      packages=['src','model','datasets'],
+setup(name='deepframeinterpolation', 
+      version='0.0.1', 
+      description='Deep Frame Interpolation', 
+      author='Arjun-Arora', 
+      author_email='me@GitHub.com', 
+      packages=['src','model','datasets'], 
       zip_safe=False)
